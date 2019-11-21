8c4371605e9f45e3e44ed0bfd37195137b55f833
Tsering Lama <tseringlama@pursuit.org>
2019-11-21 11:34:14 -0500

    successfully updates the font

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/SettingViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/SettingViewController.swift
@@ -17,52 +17,41 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var previewFont: UILabel!
     @IBOutlet weak var updateButton: UIButton!
     
+    var data: Movie!
     
-    var fontSize: Double = 16.0 {
+    var fontSize: Double = 16.0   {
         didSet {
-            previewFont.text = "Font Size \(changeStepper.value) "
+            previewFont.text = "Font Size \(String(format: "%0.f", changeStepper.value))"
         }
     }
-    
-    var fontSlide: Float = 16.0 {
-        didSet {
-            previewFont.text = "Font Size \(String(format: "%0.f", changeSlider.value))"
-        }
-    }
-    
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSlider()
         configureStepper()
-        
     }
     
     func configureStepper() {
         changeStepper.minimumValue = 5.0
-        changeStepper.maximumValue = 21.0
+        changeStepper.maximumValue = 30.0
         changeStepper.stepValue = 1.0
-        changeStepper.value = 14.0
+        changeStepper.value = 16.0
     }
     
     func configureSlider() {
         changeSlider.minimumValue = 5.0
-        changeSlider.maximumValue = 21.0
-        changeSlider.value = 14.0
+        changeSlider.maximumValue = 30.0
+        changeSlider.value = 16.0
     }
-    
     
     @IBAction func sliderTouched(_ sender: UISlider) {
-        fontSlide = sender.value
+        fontSize = Double(sender.value)
         changeStepper.value = Double(sender.value)
+
     }
-    
     
     @IBAction func stepperTouched(_ sender: UIStepper) {
         fontSize = sender.value
         changeSlider.value = Float(sender.value)
     }
-    
-
 }
--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -12,33 +12,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var data = [Movie]()
+    var datas = [Movie]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var fontSize: Double = 16.0 {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self 
-        data = Movie.allMovies
+        datas = Movie.allMovies
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc1 = segue.destination as? SettingViewController else {
+            fatalError()
+        }
+        vc1.fontSize = fontSize
     }
     
     @IBAction func updateFont (segue: UIStoryboardSegue) {
         guard let vc = segue.source as? SettingViewController else {
             fatalError()
         }
-        
+        fontSize = Double(vc.fontSize)
     }
-    
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return datas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
-        let movie = data[indexPath.row]
+        let movie = datas[indexPath.row]
         cell.textLabel?.text = movie.name
         cell.detailTextLabel?.text = movie.year.description
+        cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: CGFloat(fontSize) )
+        cell.detailTextLabel?.font = UIFont(name: "HelveticaNeue-Light", size: CGFloat(fontSize))
         return cell
     }
 }
